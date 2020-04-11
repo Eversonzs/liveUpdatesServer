b5012dc2251ac3ac2eabe371aac13190dd756041
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-11 16:00:43 -0600

    .env added and setting for server PORT

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const express = require('express');
 const timeout = require('connect-timeout');
 const morganLogger = require('morgan');
 const bp = require("body-parser");
+const SETTINGS = require('./settings');
 
 const app = express();
+
+const { PORT } = SETTINGS;
 
 app.use(bp.urlencoded({ extended: false }));
 app.use(bp.json());
 app.use(timeout('3000s'));
 app.use(morganLogger('dev'));
 
-app.listen(3000, function () {
-    console.log('Express server listening on port 3000');
+app.listen(PORT, function () {
+    console.log(`Express server listening on port ${PORT}`);
 });
 
 app.use((req, res) => {
-  res.status(200).json({ message: `Server is running.` });
+  res.status(200).json({ message: `Server is running on port ${PORT}` });
 });
 
 module.exports = app;
---
+++ settings.js
@@ -0,0 +1,10 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (NODE_ENV === 'development' || NODE_ENV === 'test') {
+  require('dotenv').config();
+}
+module.exports = {
+  APP_NAME: process.env.APP_NAME || 'liveupdatesserver',
+  PORT: process.env.PORT || 5000,
+  NODE_ENV: process.env.NODE_ENV || 'production',
+};
