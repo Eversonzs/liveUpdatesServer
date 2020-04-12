f1beed14303a92b622ad2ff298dcaab41ef1a373
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-12 12:24:09 -0600

    single quote rule for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
     "extends": "standard",
     "rules": {
+        "quotes": [
+            "error",
+            "single",
+            { "avoidEscape": true, "allowTemplateLiterals": false }
+        ],
         "semi": [2, "always"],
         "comma-dangle": ["error", {
             "arrays": "always-multiline",
