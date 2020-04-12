9ad8e76dac2abb119035a515d2f7f000eebc4d38
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-12 12:57:23 -0600

    adding logger for postgresql service

--- src/services/postgresql/postgresqlConnection.js
+++ src/services/postgresql/postgresqlConnection.js
@@ -1,12 +1,13 @@
 const { Pool } = require('pg');
 const SETTINGS = require('../../../settings');
+const logger = require('../../../logger')('postgresql-connection');
 
 const pool = new Pool({
   connectionString: SETTINGS.POSTGRESQL_CONNECTION,
 });
 
 if (pool) {
-  console.log('Postgresql connected.');
+  logger.info('Postgresql connected.');
 }
 
 module.exports = pool;
