4eb3ae62955f4afebd474c16f3d77b349ed1d700
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-12 12:10:48 -0600

    adding ESLint rules

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "extends": "standard",
+    "rules": {
+        "semi": [2, "always"],
+        "comma-dangle": ["error", {
+            "arrays": "always-multiline",
+            "objects": "always-multiline",
+        }]
+    }
+};
