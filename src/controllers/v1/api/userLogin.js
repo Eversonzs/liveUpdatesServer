39554a0552fe4d1db0a2aeaabce9f9d616eb623e
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-12 13:24:31 -0600

    adding bcryptjs library to encrypt password

--- src/controllers/v1/api/userLogin.js
+++ src/controllers/v1/api/userLogin.js
@@ -1,9 +1,17 @@
 const logger = require('../../../../logger')('controller-userLogin');
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-  userLogin (req, res) {
+  async userLogin (req, res) {
     const { username, password } = req.body;
     logger.info('username, password', username, password);
+    try {
+        const encryptedPassword = await bcrypt.hash(password, 8);
+        logger.info('encryptedPassword', encryptedPassword);
+    } catch (error) {
+        logger.error('Error encrypting password.');
+    }
+
     res.status(200).json({ message: 'Done', code: 200 });
   },
 };
