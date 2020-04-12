8601fa429202f944ae1099f6b648e5d667893f35
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-12 12:03:55 -0600

    adding db_uri to env file and configs

--- settings.js
+++ settings.js
@@ -7,4 +7,5 @@ module.exports = {
   APP_NAME: process.env.APP_NAME || 'liveupdatesserver',
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV || 'production',
+  DB_URI: process.env.DB_URI,
 };
