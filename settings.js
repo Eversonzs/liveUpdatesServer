5264026acf5126a842cdd9707058afbb134d7a22
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-12 12:21:54 -0600

    eslint rules added

--- settings.js
+++ settings.js
@@ -3,6 +3,7 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 if (NODE_ENV === 'development' || NODE_ENV === 'test') {
   require('dotenv').config();
 }
+
 module.exports = {
   APP_NAME: process.env.APP_NAME || 'liveupdatesserver',
   PORT: process.env.PORT || 5000,
