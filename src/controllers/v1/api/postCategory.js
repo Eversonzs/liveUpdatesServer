8231895dd31d1b1790cb9ceee8f7fcbde6c6a5d2
Everson Lenin Zelaya Sanchez <eversonzs@hotmail.com>
2020-04-12 23:29:05 -0600

    call function to create post category on controller

--- src/controllers/v1/api/postCategory.js
+++ src/controllers/v1/api/postCategory.js
@@ -1,6 +1,7 @@
 const { isEmpty } = require('lodash');
 const logger = require('../../../../logger')('controller-postCategory');
 const { errorMissingName } = require('../../../helpers/responseCode/customizeResponseCode/postCategory');
+const { createPostCategory } = require('../postgresql/postCategory');
 
 module.exports = {
   createPostCategory (req, res) {
@@ -12,25 +13,12 @@ module.exports = {
         .json({ code: errorMissingName.code, message: errorMissingName.message });
     }
     try {
-        const userLogin = await getUserByEmail(email);
-        let responseCode = userLogin.code || userFound.code;
-        let responseMessage = userLogin.message || userFound.message;
-        if (responseCode === 404) {
-            return res.status(responseCode)
-              .json({ code: responseCode, message: responseMessage });
-        }
-
-        // Compare if encrypted password match
-        const passwordMatch = bcrypt.compareSync(password, userLogin.password);
-        if (passwordMatch) {
-          return res.status(responseCode)
-            .json({ code: 200, message: responseMessage, user: userLogin });
-        }
-
-        return res.status(userUnauthorized.code)
-          .json({ code: userUnauthorized.code, message: userUnauthorized.message });
+        const postCategoryCreated = await createPostCategory(name, description);
+        logger.debug('postCategoryCreated: ', postCategoryCreated);
+        return res.status(postCategoryCreated.code)
+          .json({ code: postCategoryCreated.code, message: postCategoryCreated.message });
     } catch (error) {
-        logger.error(`Error retrieving user data: ${error}`);
+        logger.error(`There was an error: ${error}`);
         return res.status(400).json({ code: 400, message: error });
     }
   },
